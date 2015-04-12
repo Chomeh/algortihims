b848e1b9bdbda1cfd202d6d33158c7d4d73b168f
Jim Vella <whoisjim@hotmail.com>
2015-04-12 08:58:55 +0800

    https://start.spring.io/

---
+++ src/main/java/au/id/jim-vella/practice/AlgorithimsApplication.java
@@ -0,0 +1,12 @@
+package au.id.jim-vella.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithimsApplication.class, args);
+    }
+}
---
+++ src/test/java/au/id/jim-vella/practice/AlgorithimsApplicationTests.java
@@ -0,0 +1,16 @@
+package au.id.jim-vella.practice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AlgorithimsApplication.class)
+public class AlgorithimsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
